821792e9304e757648c38c5778e4c1be86ae3840
Juan Antonio Breña Moral <bren@juanantonio.info>
2024-01-19 15:45:01 +0000

    adding pipeline

--- src/test/java/com/example/demo/KafkaIntegrationTest.java
+++ src/test/java/com/example/demo/KafkaIntegrationTest.java
@@ -1,23 +1,17 @@
 package com.example.demo;
 
-import org.apache.kafka.clients.consumer.ConsumerConfig;
-import org.apache.kafka.clients.producer.ProducerConfig;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.test.web.server.LocalServerPort;
-import org.springframework.kafka.test.EmbeddedKafkaBroker;
-import org.springframework.kafka.test.utils.ContainerTestUtils;
-import org.springframework.kafka.test.utils.KafkaTestUtils;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.testcontainers.containers.KafkaContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
-import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -36,24 +30,9 @@ public class KafkaIntegrationTest {
     @Autowired
     private TestRestTemplate restTemplate;
 
-    //@Autowired
-    //private EmbeddedKafkaBroker embeddedKafka;
-
     @Test
     void produceAndConsumeMessage() throws Exception {
 
-        // Configure Kafka properties for the test
-        //Map<String, Object> consumerProps = KafkaTestUtils.consumerProps("testGroup", "true", embeddedKafka);
-        //consumerProps.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
-
-        // Wait until the Kafka container is ready
-        //ContainerTestUtils.waitForAssignment(embeddedKafka.getPartitionsPerTopic(), port);
-
-        // Perform the test
-        //String response = restTemplate.getForObject("http://localhost:" + port + "/kafka/produce", String.class);
-
-        // Assert the response
-        //assertThat(response).isEqualTo("Message sent successfully!");
         assertThat(true).isEqualTo(true);
     }
 }
